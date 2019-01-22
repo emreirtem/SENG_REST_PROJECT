79c519389e14f3ea4fb844583848b39750cc21bb
pelinsenkula <pelinsenkula@hotmail.com>
2019-01-22 15:55:54 +0300

    student.html, student.js

--- UI/js/page.student.js
+++ UI/js/page.student.js
@@ -12,44 +12,111 @@ var validation = {
     }
 };
 
-var validateStudentCallback = function(r){
-	if(r["isStudentValid"]){		
-		student_id = $( "#student_id" ).val()
-		StudentService.getStudentById(student_id).then(r=>getStudentByIdCallback(r))		
-	}else{
-		alert("Invalid user.");
-	}
-}  
-
-var getStudentByIdCallback = function(r){			
-		$("#student_name").text(r["student_name"]);	
-} 
-
 
 $( "#login" ).click(function() {
 	student_id = $( "#student_id" ).val()
 	if (!validation.isNumber(student_id)){
 		alert('Student id must be a number.')
 	} else {
-		StudentService.validateStudentById(student_id).then(r=>validateStudentCallback(r))
+		StudentService.validateStudentById(student_id)
+		.then(function(r){
+				if(r["isStudentValid"]){		
+					student_id = $( "#student_id" ).val()
+					StudentService.getStudentById(student_id)
+					.then(
+						function(r){
+							$("#student_name").text(r["student_name"])		
+							
+							if (r["approval_status"] == "ACCEPT_WAITING") {
+								// if waiting for approval, no topic, no advisor button
+								AdvisorService.getAdvisorById(r["advisor_id"]).then(r=>$( "#advisor_name" ).text(r["advisor_name"]))
+								r["approval_status"] = "WAITING FOR ACCEPTANCE"
+								r["thesis_topic"] = "You can decide the topic after advisor approval"
+								$( "#choose_advisor" ).hide()
+								$( "#choose_topic" ).hide()
+							} else if (r["approval_status"] == "ACCEPTED"){
+								// if approval accepted, no advisor button only topic button
+								AdvisorService.getAdvisorById(r["advisor_id"]).then(r=>$( "#advisor_name" ).text(r["advisor_name"]))
+								if (r["thesis_topic"] == null) {
+									r["thesis_topic"] = "Not Determined Yet"
+								}
+								$( "#choose_advisor" ).hide();
+								$( "#choose_topic" ).show()								
+								
+							} else if (r["approval_status"] == "REJECTED"){
+								// if approval rejected, only advisor button
+								AdvisorService.getAdvisorById(r["advisor_id"]).then(r=>$( "#advisor_name" ).text(r["advisor_name"]))
+								r["thesis_topic"] = "You can decide the topic after advisor approval"
+								$( "#choose_advisor" ).show();
+								$( "#choose_topic" ).hide()
+							} else {
+								// if none of above, only advisor button, student didnt choose anything yet
+								$( "#advisor_name" ).text("Advisor is not determined yet.")
+								r["approval_status"] = " - "
+								r["thesis_topic"] = "Please determine an advisor first."
+								$( "#choose_advisor" ).show();
+								$( "#choose_topic" ).hide()
+							}		
+							
+							$( "#approval_status" ).text(r["approval_status"])							
+							$( "#thesis_topic" ).text(r["thesis_topic"])							
+						}
+					)	
+					
+				}else{
+					alert("Invalid user.");
+				}
+			})
 	}
 });
 
 
 $( "#choose_advisor" ).click(function() {
-	AdvisorService.getAllAdvisors().then(r=>getAllAdvisorsCallback(r))
+	AdvisorService.getAllAdvisors()
+	.then(function(r){
+				$("#advisor_list_group").html('<h1>Advisors List</h1>')
+				for (i in r) {	
+					if (r[i]["availability"]) {
+						var availability = 'Available'
+					} else {
+						var availability = 'Not Available'
+					}
+					
+					if (r[i]["field_of_interest"] == null) {
+						r[i]["field_of_interest"] = 'No field of interest'
+					}
+					$( "#advisor_list_group").append( '<button type="button" class="list-group-item list-group-item-action choose-advisor" data-id="'+r[i]["advisor_id"]+'">'+r[i]["advisor_id"]+' - '+r[i]["advisor_name"]+' - '+availability+' - '+r[i]["field_of_interest"]+'</button>' );
+				}
+			}
+		)
+});
+
+$(document).on('click', '.choose-advisor', function(){ 
+	advisor_id = $(this).attr("data-id")
+	student_id = $( "#student_id" ).val()
+	
+	StudentService.sendApprovalRequest(student_id, advisor_id)
+	.then(
+		function(r){
+			if (r["success"]) {
+				alert(" choose advisor success")
+			} else {
+				if (r["remoteAdvisorService"] == null) {
+					alert(r["desc"])
+				} else {
+					alert(r["remoteAdvisorService"]["desc"])
+				}			
+			}
+		})	
 });
 
 
-var getAllAdvisorsCallback = function(r){
-	$("#advisor_list_group").html('<h1>Advisors List</h1>')
-	for (i in r) {		
-		$( "#advisor_list_group").append( '<button type="button" class="list-group-item list-group-item-action">'+r[i]["advisor_id"]+' - '+r[i]["advisor_name"]+'</button>' );
-	
-	}	
-		
+$( "#send_topic" ).click(function() {
+	thesis_topic_area = $( "#thesis_topic_area" ).val()
+	alert(thesis_topic_area)
+});
 
-} 
+
 
 
 
